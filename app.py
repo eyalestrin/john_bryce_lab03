380790aa6959fb93013244b12bc68db09d48fecd
Eyal Estrin <33989266+eyalestrin@users.noreply.github.com>
2022-08-21 15:54:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import boto3
 
-ec2 = boto3.resource('ec2', region_name = 'us-east-1')
+//ec2 = boto3.resource('ec2', region_name = 'us-east-1')
+ec2 = boto3.resource('ec2')
 
 for instance in ec2.instances.all():
     bool_variable = False
