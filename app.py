222727795bce6fd28d77fea6c177633db3026c79
Eyal Estrin <33989266+eyalestrin@users.noreply.github.com>
2022-09-03 11:58:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import boto3
 import os
 import time
 
-ec2 = boto3.resource('ec2', region_name = os.environ.get('REGION'))
+#ec2 = boto3.resource('ec2', region_name = os.environ.get('REGION'))
+ec2 = boto3.resource('ec2', region_name = 'us-east-1')
 
 def get_ec2_status():
     for instance in ec2.instances.all():
