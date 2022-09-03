8a74a5ff9723f03b1169538c3cfde38845827656
Eyal Estrin <33989266+eyalestrin@users.noreply.github.com>
2022-09-03 15:04:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,10 @@ import boto3
 import os
 import time
 
-ec2 = boto3.resource('ec2', region_name = 'us-east-1')
-#ec2 = boto3.resource('ec2', region_name = os.environ.get('REGION'))
-#ec2 = boto3.client('ec2', region_name = os.environ.get('REGION'))
+ec2 = boto3.resource('ec2', region_name = os.environ.get('REGION'))
 
 def get_ec2_status():
     for instance in ec2.instances.all():
-#    for instance in ec2.run_instances(MinCount=1,MaxCount=123):
         bool_variable = False
         for tag in instance.tags:
             if tag['Key'] == "k8s.io/role/master" and tag['Value'] == "1" and instance.state["Code"] == 16:
@@ -19,9 +16,6 @@ def get_ec2_status():
                     print(f"Instance ID:",instance.id, ", Instance Name:", tag['Value'])
 
 get_parameter_from_jenkins = os.environ.get('INTERVAL')
-
-#region_name = os.environ.get('REGION')
-#print(f"Region:",region_name)
 
 while True:
     get_ec2_status()
